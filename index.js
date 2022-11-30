69c008b5aa2d083c146d9e5db39fa91632ee234e
Oscar Hackman <ohackman1@gmail.com>
2022-11-30 23:13:33 +0200

    setup

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import { create } from "ipfs-http-client";
+import * as dotenv from 'dotenv' 
+dotenv.config()
+
+let INFURA_ID = process.env.INFURA_ID
+let INFURA_SECRET_KEY = process.env.INFURA_SECRET
  
 async function ipfsClient() {
-  const ipfs = create({host: "ipfs.infura.io", port: 5001, protocol: "https"});
+  const auth = 'Basic ' + Buffer.from(INFURA_ID + ':' + INFURA_SECRET_KEY).toString('base64');
+  const ipfs = create({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    headers: {
+        authorization: auth,
+    },
+  });
   return ipfs;
 }
 
 async function test() {
   let ipfs = await ipfsClient();
 
-  let result = await ipfs.add('Hello World');
-  console.log(result);
+  let { cid } = await ipfs.add('Hello World');
+  console.log(cid);
 }
 
 test()
